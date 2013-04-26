bf8acb56e9c6b4d566649d6bbc976e5d6249ee84
Matt Styles <matt@veryfizzyjelly.com>
2013-04-26 22:54:32 +0100

    Added a multiple source file test

---
+++ test/multiple_test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var grunt = require('grunt');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports.jsonmin = {
+  setUp: function(done) {
+    // setup here if necessary
+    done();
+  },
+  tearDown: function(done) {
+    // tear down test here if necessary
+    done();
+  },
+  stripAll: function(test) {
+    test.expect(1);
+
+    var actual = grunt.file.read('tmp/multiple.json');
+    var expected = grunt.file.read('test/expected/multiplemin.json');
+
+    test.equal(actual, expected, 'should join multiple json files and should strip comments and whitespace');
+
+    test.done();
+  }
+};
